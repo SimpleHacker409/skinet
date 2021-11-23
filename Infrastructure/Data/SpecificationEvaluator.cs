a560a0733a8ee62dbdf57cae2cbb14cbd05e7d5c
SimpleHacker409 <manjunath.siddesh@studenti.unimi.it>
2021-11-24 00:34:45 +0100

    added paging & searching

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -30,6 +30,11 @@ namespace Infrastructure.Data
                 query = query.OrderByDescending(spec.OrderByDecending); // p => p.ProductTypeId == id
             }
 
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
