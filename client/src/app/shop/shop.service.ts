e21d187084ce716175ce8d8a5fd545fe3808d423
SimpleHacker409 <manjunath.siddesh@studenti.unimi.it>
2021-12-12 17:39:59 +0100

    Added Pagination

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
+import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
+import {map} from 'rxjs/operators';
+import { shopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(shopPrams: shopParams) {
+    let params = new HttpParams();
+    if(shopPrams.brandId !== 0) {
+      params = params.append('brandId', shopPrams.brandId.toString());
+    }
+    if(shopPrams.typeId !== 0) {
+      params = params.append('typeId', shopPrams.typeId.toString());
+    }
+
+      params= params.append('sort', shopPrams.sort);
+      params = params.append('pageIndex', shopParams.pageNumber.toString());
+      params = params.append('pageIndex', shopParams.pageSize.toString());
+
+    return this.http.get<IPagination>(this.baseUrl + 'products',{observe:'response',params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+}
